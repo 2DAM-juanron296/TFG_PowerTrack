5a52f9765a1c6aa7e5af688a7e3588a215263a60
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-04-21 16:24:29 +0200

    Auth - Falta autenticación

---
+++ Backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Credenciales Incorrectas'], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login exitoso',
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user        
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Sesión cerrada']);
+    }
+}
