9a203964e4002e830b4c9d9d2145a68371ced8a6
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-03 16:41:04 +0200

    Refactorización de páginas y control

--- WebFrontend/src/components/Routines.jsx
+++ WebFrontend/src/components/Routines.jsx
@@ -1,4 +1,86 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { deleteRoutine, fetchRoutines } from "../api/routines";
+
 export function Routines() {
+  const [routines, setRoutines] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getRoutines();
+    return () => {};
+  }, []);
+
+  const getRoutines = async () => {
+    try {
+      const [data, res] = await fetchRoutines();
+
+      if (res) {
+        toast.error(data.message, {
+          style: {
+            background: "#333",
+            color: "#fff",
+            fontFamily: "Inter",
+            fontWeight: 400,
+          },
+        });
+        return;
+      }
+
+      setRoutines(data.routines);
+    } catch (error) {
+      console.error("Error al obtener las rutinas", error);
+      toast.error("Error al obtener las rutinas", {
+        style: {
+          background: "#333",
+          color: "#fff",
+          fontFamily: "Inter",
+          fontWeight: 400,
+        },
+      });
+    }
+  };
+
+  const handleDelete = (id) => async () => {
+    try {
+      const [data, res] = await deleteRoutine(id);
+
+      if (res) {
+        toast.error(data.message, {
+          style: {
+            background: "#333",
+            color: "#fff",
+            fontFamily: "Inter",
+            fontWeight: 400,
+          },
+        });
+        return;
+      }
+
+      toast.success(data.message, {
+        style: {
+          background: "#333",
+          color: "#fff",
+          fontFamily: "Inter",
+          fontWeight: 400,
+        },
+      });
+
+      await getRoutines();
+    } catch (error) {
+      console.error("Error al eliminar la rutina", error);
+      toast.error("Error al eliminar la rutina", {
+        style: {
+          background: "#333",
+          color: "#fff",
+          fontFamily: "Inter",
+          fontWeight: 400,
+        },
+      });
+    }
+  };
+
   return (
     <div className="p-8">
       <div
@@ -8,7 +90,20 @@ export function Routines() {
         Lista de Rutinas
       </div>
 
-      <div className="flex justify-center mt-10">
+      <div className="flex justify-end mb-2 me-2">
+        <button
+          className="bg-[#FF9811] text-black px-3 py-2 rounded-md cursor-pointer"
+          style={{ fontWeight: 600 }}
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/home/routines/create");
+          }}
+        >
+          Crear
+        </button>
+      </div>
+
+      <div className="flex justify-center">
         <table className="w-4xl mx-auto text-white border border-[#fff] rounded-md overflow-hidden">
           <thead>
             <tr className="bg-[#0F0F0F]">
@@ -19,33 +114,42 @@ export function Routines() {
                 Descripción
               </th>
               <th className="px-6 py-4 border-b border-[#333] text-center">
+                Usuario
+              </th>
+              <th className="px-6 py-4 border-b border-[#333] text-center">
                 Acciones
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="hover:bg-[#1d1d1d]">
-              <td className="px-6 py-4 border-b border-[#333] text-center">
-                Rutina 1
-              </td>
-              <td className="px-6 py-4 border-b border-[#333] text-center">
-                Rutina para principiantes
-              </td>
-              <td className="px-6 py-4 border-b border-[#333] text-center">
-                <button
-                  className="bg-[#FF9811] text-black p-2 rounded-md cursor-pointer"
-                  style={{ fontWeight: 600 }}
-                >
-                  Editar
-                </button>
-                <button
-                  className="bg-[#FF9811] text-black p-2 rounded-md ml-10 cursor-pointer"
-                  style={{ fontWeight: 600 }}
-                >
-                  Eliminar
-                </button>
-              </td>
-            </tr>
+            {routines.map((routine) => (
+              <tr key={routine.id} className="hover:bg-[#1d1d1d]">
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  {routine.name}
+                </td>
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  {routine.description}
+                </td>
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  {routine.user_username}
+                </td>
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  <button
+                    className="bg-[#FF9811] text-black px-3 py-2 rounded-md cursor-pointer"
+                    style={{ fontWeight: 600 }}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className="bg-[#FF9811] text-black p-2 rounded-md ml-10 cursor-pointer"
+                    style={{ fontWeight: 600 }}
+                    onClick={handleDelete(routine.id)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- WebFrontend/src/components/Users.jsx
+++ WebFrontend/src/components/Users.jsx
@@ -1,41 +1,26 @@
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { deleteUser, fetchUsers } from "../api/users";
 
 export function Users() {
   const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  const MySwal = withReactContent(Swal);
 
   useEffect(() => {
-    const getUsers = async () => {
-      try {
-        const token = localStorage.getItem("userToken");
+    getUsers();
+    return () => {};
+  }, []);
 
-        const response = await fetch("http://192.168.1.132:8000/api/users", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
+  const getUsers = async () => {
+    try {
+      const [data, res] = await fetchUsers();
 
-        const data = await response.json();
-
-        if (!response.ok) {
-          toast.error(data.message, {
-            style: {
-              background: "#333",
-              color: "#fff",
-              fontFamily: "Inter",
-              fontWeight: 400,
-            },
-          });
-          return;
-        }
-
-        setUsers(data.users);
-      } catch (error) {
-        console.error("Error al obtener los usuarios", error);
-        toast.error("Error al obtener los usuarios", {
+      if (res) {
+        toast.error(data.message, {
           style: {
             background: "#333",
             color: "#fff",
@@ -43,10 +28,75 @@ export function Users() {
             fontWeight: 400,
           },
         });
+        return;
       }
-    };
-    getUsers();
-  }, []);
+
+      setUsers(data.users);
+    } catch (error) {
+      console.error("Error al obtener los usuarios", error);
+      toast.error("Error al obtener los usuarios", {
+        style: {
+          background: "#333",
+          color: "#fff",
+          fontFamily: "Inter",
+          fontWeight: 400,
+        },
+      });
+    }
+  };
+
+  const handleDelete = (id) => async () => {
+    const result = await MySwal.fire({
+      title: "¿Estás seguro de eliminar a este usuario?",
+      icon: "warning",
+      showCancelButton: true,
+      background: "#010410",
+      confirmButtonColor: "#25AEA6",
+      cancelButtonColor: "#ff0000",
+      confirmButtonText: "Eliminar",
+      cancelButtonText: "Cancelar",
+      color: "white",
+    });
+
+    if (!result.isConfirmed) return;
+
+    try {
+      const [data, res] = await deleteUser(id);
+
+      if (res) {
+        toast.error(data.message, {
+          style: {
+            background: "#333",
+            color: "#fff",
+            fontFamily: "Inter",
+            fontWeight: 400,
+          },
+        });
+        return;
+      }
+
+      toast.success(data.message, {
+        style: {
+          background: "#333",
+          color: "#fff",
+          fontFamily: "Inter",
+          fontWeight: 400,
+        },
+      });
+
+      await getUsers();
+    } catch (error) {
+      console.error("Error al eliminar el usuario", error);
+      toast.error("Error al eliminar el usuario", {
+        style: {
+          background: "#333",
+          color: "#fff",
+          fontFamily: "Inter",
+          fontWeight: 400,
+        },
+      });
+    }
+  };
 
   return (
     <div className="p-8">
@@ -57,61 +107,69 @@ export function Users() {
         Lista de Usuarios
       </div>
 
-      {users.length === 0 ? (
-        <div className="text-center text-white text-lg mt-10">
-          No hay usuarios registrados.
-        </div>
-      ) : (
-        <div className="flex justify-center mt-10">
-          <table className="w-4xl mx-auto text-white border border-[#fff] rounded-md overflow-hidden">
-            <thead>
-              <tr className="bg-[#0F0F0F]">
-                <th className="px-6 py-4 border-b border-[#333] text-center">
-                  Nombre
-                </th>
-                <th className="px-6 py-4 border-b border-[#333] text-center">
-                  Username
-                </th>
-                <th className="px-6 py-4 border-b border-[#333] text-center">
-                  Email
-                </th>
-                <th className="px-6 py-4 border-b border-[#333] text-center">
-                  Acciones
-                </th>
+      <div className="flex justify-end mb-2 me-2">
+        <button
+          className="bg-[#FF9811] text-black px-3 py-2 rounded-md cursor-pointer"
+          style={{ fontWeight: 600 }}
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/home/users/create");
+          }}
+        >
+          Crear
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <table className="w-4xl mx-auto text-white border border-[#fff] rounded-md overflow-hidden">
+          <thead>
+            <tr className="bg-[#0F0F0F]">
+              <th className="px-6 py-4 border-b border-[#333] text-center">
+                Nombre
+              </th>
+              <th className="px-6 py-4 border-b border-[#333] text-center">
+                Username
+              </th>
+              <th className="px-6 py-4 border-b border-[#333] text-center">
+                Email
+              </th>
+              <th className="px-6 py-4 border-b border-[#333] text-center">
+                Acciones
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id} className="hover:bg-[#1d1d1d]">
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  {user.username}
+                </td>
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  {user.email}
+                </td>
+                <td className="px-6 py-4 border-b border-[#333] text-center">
+                  <button
+                    className="bg-[#FF9811] text-black px-3 py-2 rounded-md cursor-pointer"
+                    style={{ fontWeight: 600 }}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className="bg-[#FF9811] text-black p-2 rounded-md ml-10 cursor-pointer"
+                    style={{ fontWeight: 600 }}
+                    onClick={handleDelete(user.id)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id} className="hover:bg-[#1d1d1d]">
-                  <td className="px-6 py-4 border-b border-[#333] text-center">
-                    {user.name}
-                  </td>
-                  <td className="px-6 py-4 border-b border-[#333] text-center">
-                    {user.username}
-                  </td>
-                  <td className="px-6 py-4 border-b border-[#333] text-center">
-                    {user.email}
-                  </td>
-                  <td className="px-6 py-4 border-b border-[#333] text-center">
-                    <button
-                      className="bg-[#FF9811] text-black p-2 rounded-md cursor-pointer"
-                      style={{ fontWeight: 600 }}
-                    >
-                      Editar
-                    </button>
-                    <button
-                      className="bg-[#FF9811] text-black p-2 rounded-md ml-10 cursor-pointer"
-                      style={{ fontWeight: 600 }}
-                    >
-                      Eliminar
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
