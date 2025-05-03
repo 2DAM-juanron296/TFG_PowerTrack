ba14831585adacfb5743c23868f6c6c1e064f1d6
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-03 16:42:48 +0200

    Nuevas migraciones - Rutinas y Ejercicios

---
+++ Backend/database/migrations/2025_05_02_161442_create_routine_exercises_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routine_exercises', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sets')->nullable();
+            $table->integer('reps')->nullable();
+            $table->double('weight')->nullable();
+            $table->integer('order');
+
+            $table->unsignedBigInteger('routine_id');
+            $table->foreign('routine_id')
+                  ->references('id')
+                  ->on('routines')
+                  ->onDelete('cascade');
+
+            $table->unsignedBigInteger('exercise_id');
+            $table->foreign('exercise_id')
+                  ->references('id')
+                  ->on('exercises')
+                  ->onDelete('cascade');
+
+            $table->unique(['routine_id', 'order']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routine_exercises');
+    }
+};
