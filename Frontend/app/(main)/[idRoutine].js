1ee2cfa6f2952ce1a88be5747e838bcb06922010
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-07 23:47:52 +0200

    Visualización de ejercicios con sus series concretas

--- Frontend/app/(main)/[idRoutine].js
+++ Frontend/app/(main)/[idRoutine].js
@@ -1,10 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, useLocalSearchParams } from "expo-router";
-import { Text, View, Animated, Pressable, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  Animated,
+  Pressable,
+  FlatList,
+  experimental_LayoutConformance,
+} from "react-native";
 import { styled } from "nativewind";
 import { fetchRoutineExercises } from "../../context/api/exercises";
 import Toast from "react-native-toast-message";
 import { HomeIcon } from "../../components/Icons";
+import { fetchExerciseSets } from "../../context/api/sets";
 
 export default function DetailRoutine() {
   const StyledPresable = styled(Pressable);
@@ -42,10 +50,36 @@ export default function DetailRoutine() {
           visibilityTime: 5000,
         });
 
+        const setsResponse = await Promise.all(
+          data.exercises.map(async (ex) => {
+            const [dataSet, resSet] = await fetchExerciseSets(ex.id);
+
+            if (resSet) {
+              Toast.show({
+                type: "error",
+                text1: "Error",
+                text2: dataSet.message,
+                text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+                text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+                position: "bottom",
+                animation: true,
+                visibilityTime: 5000,
+              });
+              return [];
+            }
+            return dataSet.routine_exercises_sets || [];
+          }),
+        );
+
+        const enrichedExercises = data.exercises.map((ex, index) => {
+          return { ...ex, sets: setsResponse[index] };
+        });
+
+        setExercises(enrichedExercises);
+
         console.log("Rutina", data.exercises);
         console.log("Ejercicios de la rutina", data.exercises[0].exercise.name);
-
-        setExercises(data.exercises);
+        console.log("Sets: ", setsResponse);
       };
       getRoutineExercises();
     }
@@ -62,17 +96,44 @@ export default function DetailRoutine() {
       </Link>
 
       <View className="mt-10">
+        <View className="justify-start items-start mb-5">
+          <Text
+            className="text-2xl text-[#25AEA6]"
+            style={{ fontFamily: "Inter-Bold" }}
+          >
+            Ejercicios
+          </Text>
+        </View>
         <FlatList
           data={exercises}
           keyExtractor={(exercise) => exercise.id}
           renderItem={({ item }) => (
-            <View className="items-center mb-5">
+            <View className="mb-5 border border-[#222] bg-[#0f0f0f] rounded-lg p-4">
               <Text
-                className="text-white text-xl"
+                className="text-white text-xl pb-3"
                 style={{ fontFamily: "Inter-SemiBold" }}
               >
                 {item.order}. {item.exercise.name}
               </Text>
+
+              {item.sets && item.sets.length > 0 ? (
+                item.sets.map((set) => (
+                  <Text
+                    key={set.id}
+                    className="text-white pb-1"
+                    style={{ fontFamily: "Inter-Regular", fontSize: 14 }}
+                  >
+                    Serie {set.order}: {set.reps} reps - {set.weight} kg
+                  </Text>
+                ))
+              ) : (
+                <Text
+                  className="text-white pb-1"
+                  style={{ fontFamily: "Inter-Regular", fontSize: 14 }}
+                >
+                  No hay series registradas
+                </Text>
+              )}
             </View>
           )}
         />
