dcc1fdaa653a6e399b12e55023eeb1ab2f529d0c
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-08 15:59:11 +0200

    Texto inputs negro

--- Frontend/components/Register.jsx
+++ Frontend/components/Register.jsx
@@ -1,6 +1,5 @@
 import { View, Text, TextInput, Pressable, Image } from "react-native";
 import { useState } from "react";
-import { showMessage } from "react-native-flash-message";
 import { styled } from "nativewind";
 import { useAuth } from "../context/AuthContext";
 import { Link } from "expo-router";
@@ -80,28 +79,28 @@ export function Register({ onSuccess }) {
         />
         <View className="border-2 border-[#25AEA6] rounded-xl bg-[#0F0F0F] w-80 full py-14 justify-center items-center">
           <TextInput
-            className="bg-[#54807D] rounded-md w-60 p-4 text-left"
+            className="bg-[#54807D] rounded-md w-60 p-4 text-left text-black"
             placeholder="Nombre"
             placeholderTextColor={"#222"}
             style={{ fontFamily: "Inter-SemiBold" }}
             onChangeText={setName}
           />
           <TextInput
-            className="bg-[#54807D] rounded-md mt-6 w-60 p-4 text-left"
+            className="bg-[#54807D] rounded-md mt-6 w-60 p-4 text-left text-black"
             placeholder="Username"
             placeholderTextColor={"#222"}
             style={{ fontFamily: "Inter-SemiBold" }}
             onChangeText={setUsername}
           />
           <TextInput
-            className="bg-[#54807D] rounded-md mt-6 w-60 p-4 text-left"
+            className="bg-[#54807D] rounded-md mt-6 w-60 p-4 text-left text-black"
             placeholder="Email"
             placeholderTextColor={"#222"}
             style={{ fontFamily: "Inter-SemiBold" }}
             onChangeText={setEmail}
           />
           <TextInput
-            className="bg-[#54807D] rounded-md mt-6 w-60 p-4 text-left"
+            className="bg-[#54807D] rounded-md mt-6 w-60 p-4 text-left text-black"
             placeholder="Password"
             placeholderTextColor={"#222"}
             style={{ fontFamily: "Inter-SemiBold" }}
