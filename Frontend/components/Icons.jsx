293426527f905d02bbee2db68ea979d7da325e06
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-10 12:51:01 +0200

    Icono Back

--- Frontend/components/Icons.jsx
+++ Frontend/components/Icons.jsx
@@ -16,3 +16,4 @@ export const UserDefaultIcon = () => (
   <FontAwesome name="user-circle" size={80} color="#25AEA6" />
 );
 export const ChatIcon = () => <Entypo name="chat" size={24} color="#25AEA6" />;
+export const BackIcon = () => <Entypo name="back" size={24} color="#25AEA6" />;
