a5383df46bae0876a8dc57f8f5f4771304d53969
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-10 14:08:44 +0200

    Métodos para recoger y guardar rutinas

--- Frontend/context/api/routines.js
+++ Frontend/context/api/routines.js
@@ -34,3 +34,68 @@ export async function fetchDefaultRoutines() {
     return [{ data: error }, true];
   }
 }
+
+export async function fetchUserRoutines(id_user) {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/routines/${id_user}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
+
+export async function saveDefaultRoutinetoUser(request) {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/saveRoutine`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(request),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
