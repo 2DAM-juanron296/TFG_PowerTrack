d7a97e8fc4b6d1f5a722a5448ac903f5c949d636
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-10 14:09:31 +0200

    Actualización para la navegación y estilos

--- Frontend/components/RoutineCard.jsx
+++ Frontend/components/RoutineCard.jsx
@@ -1,24 +1,35 @@
 import { useEffect, useRef } from "react";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { Text, View, Animated, Pressable } from "react-native";
 import { styled } from "nativewind";
 
-export function RoutineCard() {
+export function RoutineCard({ name, description, id }) {
   const StyledPresable = styled(Pressable);
+  const router = useRouter();
 
   return (
     <View className="w-full">
       <StyledPresable
+        onPress={() => router.push(`/(main)/${id}`)}
         className="mb-3 bg-[#0f0f0f] rounded-lg p-4"
         style={{ minWidth: "100%" }}
       >
         <View className="flex-row justify-between items-center">
-          <Text
-            className="text-white text-lg"
-            style={{ fontFamily: "Inter-SemiBold" }}
-          >
-            Rutina
-          </Text>
+          <View className="flex-1">
+            <Text
+              className="text-white text-lg"
+              style={{ fontFamily: "Inter-SemiBold" }}
+            >
+              {name}
+            </Text>
+            <Text
+              className="text-gray-200 text-sm mt-1"
+              style={{ fontFamily: "Inter-Regular" }}
+              numberOfLines={2}
+            >
+              {description}
+            </Text>
+          </View>
           <StyledPresable className="bg-[#25AEA6] rounded-md p-2">
             <Text className="text-black" style={{ fontFamily: "Inter-Bold" }}>
               Comienzo
