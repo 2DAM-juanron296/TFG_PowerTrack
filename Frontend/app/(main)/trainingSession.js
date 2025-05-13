fb9864a82d596d5a014c7ebe1f57d9a131f7e29f
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-13 18:09:22 +0200

    Arhivo para sesión de entrenamiento

---
+++ Frontend/app/(main)/trainingSession.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { Screen } from "../../components/Screen";
+
+export default function TrainingSession() {
+  return (
+    <Screen>
+      <View className="justify-center items-center">
+        <Text className="text-white">Buenas tardes</Text>
+      </View>
+    </Screen>
+  );
+}
