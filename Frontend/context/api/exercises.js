540259156600cb33a1d612db1caa126746b424dc
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-13 20:40:58 +0200

    Método para obtener todos los ejercicios

--- Frontend/context/api/exercises.js
+++ Frontend/context/api/exercises.js
@@ -3,6 +3,38 @@ import { API_BASE } from "../../config";
 
 const getToken = async () => await AsyncStorage.getItem("userToken");
 
+export async function fetchExercises() {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/exercises`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
+
 export async function fetchRoutineExercises(idRoutine) {
   try {
     const token = await getToken();
