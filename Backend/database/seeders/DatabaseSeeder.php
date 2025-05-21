343c3f473c11afecba12c5fe5a5a940dd7daa2f7
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-21 18:14:18 +0200

    Creación automática de usuario administrador

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             MuscleGroupSeeder::class,
-            ExerciseSeeder::class
+            ExerciseSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
