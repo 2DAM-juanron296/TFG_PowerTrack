6b885695d185ea2b920040470752c29881edfa32
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-21 18:15:08 +0200

    Recogida de entrenos

--- Frontend/components/Main.jsx
+++ Frontend/components/Main.jsx
@@ -1,28 +1,67 @@
-import { View, Text, FlatList, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import { Screen } from "./Screen";
 import { UserDefaultIcon } from "../utils/Icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 import { TrainingCard } from "../components/training/TrainingCard";
 import { Link } from "expo-router";
+import { fetchWorkouts } from "../context/api/trainings";
+import Toast from "react-native-toast-message";
 
 export function Main() {
-  const [trainings, setTrainings] = useState(["buenas"]);
+  const [trainings, setTrainings] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const [username, setUsername] = useState("");
 
   useEffect(() => {
+    setLoading(true);
     const getData = async () => {
       try {
-        const value = await AsyncStorage.getItem("userToken");
         const user = await AsyncStorage.getItem("username");
-        const id_user = await AsyncStorage.getItem("id_user");
-
         if (user !== null) {
           setUsername(user);
         }
-        console.log("Token:", value, " Username: ", user, " ID: ", id_user);
+
+        const [data, res] = await fetchWorkouts();
+
+        if (res) {
+          console.log("Error al obtener los entrenamientos", data.message);
+          Toast.show({
+            type: "error",
+            text1: "Error",
+            text2: data.message,
+            text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+            text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+            position: "top",
+            animation: true,
+            visibilityTime: 2000,
+          });
+          return;
+        }
+
+        Toast.show({
+          type: "success",
+          text1: "Éxito",
+          text2: data.message,
+          text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+          text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+          position: "top",
+          animation: true,
+          visibilityTime: 2000,
+        });
+
+        setTrainings(data.workouts);
       } catch (error) {
         console.error("Error al obtener datos de AsyncStorage", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -31,62 +70,76 @@ export function Main() {
 
   return (
     <Screen>
-      <View className="justify-center items-center text-center pb-14 mt-16">
-        <UserDefaultIcon />
+      <View className="flex-1 justify-between pb-10">
         <View>
-          <Text
-            className="text-white text-md pt-2"
-            style={{ fontFamily: "Inter-SemiBold" }}
-          >
-            {username ? `Bienvenido, ${username}` : "Bienvenido, User"}
-          </Text>
+          <View className="justify-center items-center text-center pb-14 mt-16">
+            <UserDefaultIcon />
+            <View>
+              <Text
+                className="text-white text-md pt-2"
+                style={{ fontFamily: "Inter-SemiBold" }}
+              >
+                {username ? `Bienvenido, ${username}` : "Bienvenido, User"}
+              </Text>
+            </View>
+          </View>
+          <View className="justify-start items-start mx-10 mb-6">
+            <Text
+              className="text-2xl text-[#25AEA6]"
+              style={{ fontFamily: "Inter-Bold" }}
+            >
+              Entrenamientos
+            </Text>
+          </View>
+
+          {loading ? (
+            <View className="flex-1 justify-center items-center mb-20">
+              <ActivityIndicator size="large" color="#25AEA6" />
+              <Text
+                className="text-white text-md text-center mt-2"
+                style={{ fontFamily: "Inter-SemiBold" }}
+              >
+                Cargando...
+              </Text>
+            </View>
+          ) : trainings.length === 0 ? (
+            <View className="justify-center items-center mt-5">
+              <Text
+                className="text-white text-md"
+                style={{ fontFamily: "Inter-Bold" }}
+              >
+                NO HAY ENTRENAMIENTOS ACTUALMENTE
+              </Text>
+            </View>
+          ) : (
+            <View className="mx-10" style={{ maxHeight: "70%" }}>
+              <FlatList
+                data={trainings}
+                keyExtractor={(item, index) => `${index}`}
+                renderItem={({ item }) => (
+                  <View className="items-center mb-4">
+                    <TrainingCard workout={item} />
+                  </View>
+                )}
+                showsVerticalScrollIndicator={false}
+              />
+            </View>
+          )}
+        </View>
+
+        <View className="items-center">
+          <Link asChild href="../trainHistory">
+            <Pressable>
+              <Text
+                className="text-white"
+                style={{ fontFamily: "Inter-SemiBold" }}
+              >
+                Ver más...
+              </Text>
+            </Pressable>
+          </Link>
         </View>
       </View>
-      <View className="justify-start items-start mx-10 mb-12">
-        <Text
-          className="text-2xl text-[#25AEA6]"
-          style={{ fontFamily: "Inter-Bold" }}
-        >
-          Entrenamientos
-        </Text>
-      </View>
-      {trainings.length === 0 ? (
-        <View className="justify-center items-center">
-          <Text
-            className="text-red-600 text-md"
-            style={{ fontFamily: "Inter-Bold" }}
-          >
-            NO HAY ENTRENAMIENTOS ACTUALMENTE
-          </Text>
-        </View>
-      ) : (
-        <View className="mx-10">
-          <FlatList
-            data={trainings}
-            keyExtractor={(train) => train}
-            renderItem={() => (
-              <View className="items-center">
-                <TrainingCard />
-                <TrainingCard />
-                <TrainingCard />
-                <TrainingCard />
-                <View className="justify-center items-center mt-5">
-                  <Link asChild href="../trainHistory">
-                    <Pressable>
-                      <Text
-                        className="text-white"
-                        style={{ fontFamily: "Inter-SemiBold" }}
-                      >
-                        Ver más...
-                      </Text>
-                    </Pressable>
-                  </Link>
-                </View>
-              </View>
-            )}
-          />
-        </View>
-      )}
     </Screen>
   );
 }
