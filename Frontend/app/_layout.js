d674cbf89d11dc3f69070d27a736cc04c2bc8228
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-23 17:01:58 +0200

    Ajuste de espacio con tabs

--- Frontend/app/_layout.js
+++ Frontend/app/_layout.js
@@ -1,12 +1,27 @@
 import { Slot } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 
 export default function Layout() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <AuthProvider>
-      <Slot />
-      <Toast />
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <SafeAreaView
+          style={{
+            flex: 1,
+            backgroundColor: "#0F0F0F",
+            paddingBottom: insets.bottom,
+          }}
+        >
+          <Slot />
+          <Toast />
+        </SafeAreaView>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
