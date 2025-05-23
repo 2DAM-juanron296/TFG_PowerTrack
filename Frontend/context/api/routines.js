5989ba5d75c91b58012edb707d5d2105db3967ef
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-23 18:55:01 +0200

    Método llamada a API para nombre de rutina

--- Frontend/context/api/routines.js
+++ Frontend/context/api/routines.js
@@ -101,3 +101,35 @@ export async function createUserRoutine(request) {
     return [{ data: error }, true];
   }
 }
+
+export async function fetchRoutineName(id) {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/routine/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
