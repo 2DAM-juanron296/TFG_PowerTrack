317e0bfbe21dd9eedc5c5edc4b1117aafca5eec3
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-23 18:31:43 +0200

    Método store

--- Backend/app/Http/Controllers/WorkoutExerciseController.php
+++ Backend/app/Http/Controllers/WorkoutExerciseController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Workout_Exercise;
+use Exception;
 use Illuminate\Http\Request;
 
 class WorkoutExerciseController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $exercises = [];
+
+        try 
+        {
+            $request->validate([
+                'exercises' => 'required|array', 
+                'exercises.*.workout_id' => 'required|exists:workouts,id', 
+                'exercises.*.exercise_id' => 'required|exists:exercises,id', 
+                'exercises.*.order' => 'required|integer', 
+            ]);
+            
+            foreach ($request->exercises as $exercise) {
+                $created = Workout_Exercise::create($exercise);
+                $exercises[] = $created; 
+            }
+
+            if (empty($exercises))
+                return response()->json([
+                    'message' => 'Error al crear ejercicio para el entreno'
+                ], 400);
+            
+            return response()->json([
+                'message' => 'Ejercicios añadidos al Entrenamiento',
+                'exercises' => $exercises
+            ], 201);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Error: '.$e->getMessage()
+            ], 500);
+        }
+    }
 }
--- Backend/app/Http/Controllers/WorkoutExerciseSetController.php
+++ Backend/app/Http/Controllers/WorkoutExerciseSetController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Workout_Exercise_Set;
+use Exception;
 use Illuminate\Http\Request;
 
 class WorkoutExerciseSetController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $sets = [];
+
+        try {
+            $request->validate([
+                'sets' => 'required|array',
+                'sets.*.workout_exercise_id' => 'required|exists:workout_exercises,id',
+                'sets.*.order' => 'required|integer',
+                'sets.*.reps' => 'required|integer',
+                'sets.*.weight' => 'required|numeric|decimal:0,2',
+            ]);
+
+            foreach ($request->sets as $set) {
+                $created = Workout_Exercise_Set::create($set);
+                $sets[] = $created;
+            }
+
+            if (empty($sets))
+                return response()->json([
+                    'message' => 'Error al añadir los sets'
+                ], 400);
+
+            return response()->json([
+                'message' => 'Sets añadidos al ejercicio',
+                'sets' => $sets
+            ], 201);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Error: '.$e->getMessage()
+            ], 500);
+        }
+    }
 }
