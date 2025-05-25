6ca99061f6e00cbefb0f5e141437e05d2daf6bf0
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-25 13:30:14 +0200

    Archivo dinámico para info de rutinas y entrenos

---
+++ Frontend/app/(main)/[idWork].js
@@ -0,0 +1,232 @@
+import { useEffect, useState } from "react";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { Text, View, Pressable, FlatList, Image } from "react-native";
+import { styled } from "nativewind";
+import {
+  fetchRoutineExercises,
+  fetchWorkoutExercises,
+} from "../../context/api/exercises";
+import Toast from "react-native-toast-message";
+import { BackIcon } from "../../utils/Icons";
+import {
+  fetchExerciseSets,
+  fetchWorkoutExerciseSets,
+} from "../../context/api/sets";
+import { ExerciseImages } from "../../utils/ExerciseImages";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Detail() {
+  const StyledPresable = styled(Pressable);
+  const { idWork, from, work, date, type, workoutName } =
+    useLocalSearchParams();
+  const insets = useSafeAreaInsets();
+  const router = useRouter();
+
+  const [exercises, setExercises] = useState([]);
+
+  const getExerciseImage = (id) => {
+    return (
+      ExerciseImages[id] ||
+      require("../../assets/images/exercises/default.webp")
+    );
+  };
+
+  useEffect(() => {
+    if (idWork && work !== "workout") {
+      const getRoutineExercises = async () => {
+        const [data, res] = await fetchRoutineExercises(idWork);
+
+        if (res) {
+          Toast.show({
+            type: "error",
+            text1: "Error",
+            text2: data.message,
+            text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+            text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+            position: "top",
+            animation: true,
+            visibilityTime: 2000,
+          });
+          return;
+        }
+
+        Toast.show({
+          type: "success",
+          text1: "Éxito",
+          text2: data.message,
+          text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+          text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+          position: "top",
+          animation: true,
+          visibilityTime: 2000,
+        });
+
+        const setsResponse = await Promise.all(
+          data.exercises.map(async (ex) => {
+            const [dataSet, resSet] = await fetchExerciseSets(ex.id);
+
+            if (resSet) {
+              Toast.show({
+                type: "error",
+                text1: "Error",
+                text2: dataSet.message,
+                text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+                text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+                position: "top",
+                animation: true,
+                visibilityTime: 2000,
+              });
+              return [];
+            }
+            return dataSet.routine_exercises_sets || [];
+          }),
+        );
+
+        const enrichedExercises = data.exercises.map((ex, index) => {
+          return { ...ex, sets: setsResponse[index] };
+        });
+
+        setExercises(enrichedExercises);
+
+        console.log("Rutina", data.exercises);
+        console.log("Ejercicios de la rutina", data.exercises[0].exercise.name);
+        console.log("Sets: ", setsResponse);
+      };
+      getRoutineExercises();
+    } else if (work === "workout") {
+      const getWorkoutExercises = async () => {
+        const [data, res] = await fetchWorkoutExercises(idWork);
+
+        if (res) {
+          Toast.show({
+            type: "error",
+            text1: "Error",
+            text2: data.message,
+            text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+            text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+            position: "top",
+            animation: true,
+            visibilityTime: 2000,
+          });
+          return;
+        }
+
+        const setsResponse = await Promise.all(
+          data.exercises.map(async (ex) => {
+            const [dataSet, resSet] = await fetchWorkoutExerciseSets(ex.id);
+
+            if (resSet) {
+              Toast.show({
+                type: "error",
+                text1: "Error",
+                text2: dataSet.message,
+                text1Style: { fontFamily: "Inter-Bold", fontSize: 12 },
+                text2Style: { fontFamily: "Inter-SemiBold", fontSize: 11 },
+                position: "top",
+                animation: true,
+                visibilityTime: 2000,
+              });
+              return [];
+            }
+            return dataSet.workout_exercise_sets || [];
+          }),
+        );
+
+        const enrichedExercises = data.exercises.map((ex, index) => {
+          return { ...ex, sets: setsResponse[index] };
+        });
+
+        setExercises(enrichedExercises);
+
+        console.log("Entreno", data.exercises);
+        console.log("Ejercicios del entreno", data.exercises[0].exercise.name);
+        console.log("Sets: ", setsResponse);
+      };
+      getWorkoutExercises();
+    }
+  }, [idWork, work]);
+
+  const handleBack = () => {
+    if (from === "training" && work !== "workout") {
+      router.push("/(main)/(tabs)/training");
+    } else if (work !== "workout") {
+      router.push("/(main)/exploreRoutine");
+    } else {
+      router.push("/(main)/(tabs)/home");
+    }
+  };
+
+  return (
+    <View className="flex-1 mx-7 mt-5">
+      <StyledPresable onPress={handleBack}>
+        <BackIcon />
+      </StyledPresable>
+
+      {work === "workout" ? (
+        <View>
+          <Text
+            className="text-2xl text-white mt-5"
+            style={{ fontFamily: "Inter-Bold" }}
+          >
+            {workoutName}
+          </Text>
+          <Text
+            className="text-md text-white mt-2 mb-5"
+            style={{ fontFamily: "Inter-SemiBold" }}
+          >
+            {type} - {date}
+          </Text>
+        </View>
+      ) : (
+        <Text
+          className="text-2xl text-[#25AEA6] mt-10"
+          style={{ fontFamily: "Inter-Bold" }}
+        >
+          Ejercicios
+        </Text>
+      )}
+
+      <FlatList
+        data={exercises}
+        keyExtractor={(exercise) => exercise.id.toString()}
+        contentContainerStyle={{ paddingBottom: insets.bottom }}
+        renderItem={({ item }) => (
+          <View className="mb-3 border border-[#222] bg-[#0f0f0f] rounded-lg p-4">
+            <View className="flex-row items-center">
+              <Image
+                source={getExerciseImage(item.exercise.id)}
+                className="w-24 h-24 rounded-lg mr-[15px]"
+              />
+              <View className="flex-1">
+                <Text
+                  className="text-white text-xl pb-3"
+                  style={{ fontFamily: "Inter-SemiBold" }}
+                >
+                  {item.order}. {item.exercise.name}
+                </Text>
+                {item.sets?.length > 0 ? (
+                  item.sets.map((set) => (
+                    <Text
+                      key={set.id}
+                      className="text-white pb-1"
+                      style={{ fontFamily: "Inter-Regular", fontSize: 14 }}
+                    >
+                      Serie {set.order}: {set.reps} reps - {set.weight} kg
+                    </Text>
+                  ))
+                ) : (
+                  <Text
+                    className="text-white pb-1"
+                    style={{ fontFamily: "Inter-Regular", fontSize: 14 }}
+                  >
+                    No hay series registradas
+                  </Text>
+                )}
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
