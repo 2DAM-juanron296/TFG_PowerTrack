9903096260d9161f3b82dc8a0e962eab5af08bdc
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-25 13:30:23 +0200

    Métodos llamada a API

--- Frontend/context/api/exercises.js
+++ Frontend/context/api/exercises.js
@@ -67,6 +67,38 @@ export async function fetchRoutineExercises(idRoutine) {
   }
 }
 
+export async function fetchWorkoutExercises(idWorkout) {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/workoutExercises/${idWorkout}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
+
 export async function createExercisesRoutine(request) {
   try {
     const token = await getToken();
--- Frontend/context/api/sets.js
+++ Frontend/context/api/sets.js
@@ -38,6 +38,41 @@ export async function fetchExerciseSets(idRoutineExercise) {
   }
 }
 
+export async function fetchWorkoutExerciseSets(idWorkoutExercise) {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(
+      `${API_BASE}/workoutExerciseSets/${idWorkoutExercise}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
+
 export async function createExerciseSets(request) {
   try {
     const token = await getToken();
