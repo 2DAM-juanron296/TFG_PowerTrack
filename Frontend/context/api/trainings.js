e020a75b254542ce67eb845c9feac3abacc0e5d5
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-27 18:36:56 +0200

    Método para recoger últimos 4 entrenos

--- Frontend/context/api/trainings.js
+++ Frontend/context/api/trainings.js
@@ -2,7 +2,38 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API_BASE } from "../../config";
 
 const getToken = async () => await AsyncStorage.getItem("userToken");
-const getUserId = async () => await AsyncStorage.getItem("id_user");
+
+export async function fetchLastWorkouts() {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/workoutsLast`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
 
 export async function fetchWorkouts() {
   try {
