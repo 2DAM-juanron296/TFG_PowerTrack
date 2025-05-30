0479ee8a5960581d820fb870945616e371d5647c
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-05-30 12:55:36 +0200

    Método para obtener los datos user

--- Frontend/context/api/user.js
+++ Frontend/context/api/user.js
@@ -35,3 +35,35 @@ export async function fetchUserRoutines() {
     return [{ data: error }, true];
   }
 }
+
+export async function fetchDataUser() {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/userData`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
