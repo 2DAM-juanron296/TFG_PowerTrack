79ec9e09393f3d7ccfddbc0a961a10c05664c205
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-06-02 13:52:29 +0200

    Método conexión a API para eliminar entreno

--- Frontend/context/api/trainings.js
+++ Frontend/context/api/trainings.js
@@ -98,3 +98,34 @@ export async function createWorkout(request) {
     return [{ data: error }, true];
   }
 }
+
+export async function deleteWorkout(idWorkout) {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/deleteWorkout/${idWorkout}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
