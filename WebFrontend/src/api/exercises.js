56ac9fe71c5a9dc36ea95ae5cfd50ea281c2dbf3
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-06-10 12:21:01 +0200

    Llamada para obtener ejercicios más utilizados

--- WebFrontend/src/api/exercises.js
+++ WebFrontend/src/api/exercises.js
@@ -77,3 +77,28 @@ export async function createExercisesRoutine(request) {
     return [{ data: error }, true];
   }
 }
+
+export async function fetchTopExercises() {
+  try {
+    const token = getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/topExercises`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+    !response.ok ? (res = true) : (res = false);
+    info = [data, res];
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
