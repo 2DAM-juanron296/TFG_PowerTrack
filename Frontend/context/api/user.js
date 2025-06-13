1afdffaf97ca54c380f44185f57c01671a598769
2DAM-juanron296 <jrongil361@g.educaand.es>
2025-06-13 17:29:08 +0200

    Método de llamada a api para obtención de datos para el informe

--- Frontend/context/api/user.js
+++ Frontend/context/api/user.js
@@ -165,3 +165,35 @@ export async function updateUserProgress(reqProgress) {
     return [{ data: error }, true];
   }
 }
+
+export async function fetchInform() {
+  try {
+    const token = await getToken();
+
+    let res = false;
+    let info = [];
+
+    const response = await fetch(`${API_BASE}/getInform`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      res = true;
+    } else {
+      res = false;
+    }
+
+    info = [data, res];
+
+    return info;
+  } catch (error) {
+    return [{ data: error }, true];
+  }
+}
